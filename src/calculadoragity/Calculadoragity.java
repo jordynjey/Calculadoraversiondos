3e72704ec53871bedd963750299d3009138f5824
Usuario <Usuario@LAPTOP-831U81DV.mshome.net>
2019-12-05 13:08:58 -0500

    Rev.10

---
+++ src/calculadoragity/Calculadoragity.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoragity;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Calculadoragity {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+    public static int suma(int a ,int b){
+        int sum=a+b;
+        return sum;
+    }
+    
+}
